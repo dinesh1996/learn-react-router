72f96e85a47d0b4050f7dbb52e30fc07ff8ddc8e
Salhotra Dass <dinesh.salhotradass@ynov.com>
2023-01-29 22:54:45 +0100

    Completed the challange to create Routes and separete into a Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,20 @@
-import { Link, NavLink } from "react-router-dom";
-import logo from "../assets/red30-tech-logo.png";
+import { Link } from 'react-router-dom'
+import logo from '../assets/red30-tech-logo.png'
 
 export default function Header() {
-  const getClass = ({ isActive }) => (isActive ? "nav-active" : null);
-
   return (
-    <header className="container">
-      <Link to="/">
-        <img
-          className="logo"
-          src={logo}
-          alt="Red30 Tech logo"
-          title="Red30 Tech | Home"
-        />
-      </Link>
-
+    <header className='container'>
+      <img
+        className='logo'
+        src={logo}
+        alt='Red30 Tech logo'
+        title='Red30 Tech | Home'
+      />
       <nav>
-        <NavLink to="/" className={getClass}>
-          Home
-        </NavLink>
-        <NavLink to="/about" className={getClass}>
-          About
-        </NavLink>
-        <NavLink to="/categories" className={getClass}>
-          Categories
-        </NavLink>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/categories'}>Categories</Link>
+        <Link to={'/about'}>About</Link>
       </nav>
     </header>
-  );
+  )
 }
