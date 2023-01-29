7011c6cb187c2f2750740aadd884957cc2c7bde6
Salhotra Dass <dinesh.salhotradass@ynov.com>
2023-01-29 23:25:05 +0100

    Add useParams and display info about categories

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,24 @@
+import { useParams } from 'react-router-dom'
+import { getCategory } from '../api'
+
 export default function Category() {
+  const { categoryId } = useParams()
+  const category = getCategory(categoryId)
   return (
     <>
-      <h2>Category Sessions</h2>
-      <p>Category session content displays here...</p>
+      <h2>{category.name} Sessions</h2>
+      {category.sessions.length > 0 && (
+        <ul className='session-list'>
+          {category?.sessions.map((session) => (
+            <li key={session.id} className='session'>
+              <p className='session-name '>{session.name}</p>
+              <p>
+                {session.speaker.name} | {session.speaker.org}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
-  );
+  )
 }
